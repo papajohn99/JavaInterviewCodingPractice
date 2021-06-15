d98971a8b07f0b2dc588c4493b5f90e0e7827f74
papaJohn <priyankajari@yahoo.com>
2021-06-15 17:09:16 -0400

    Added ariplane seat reservation sample.

--- src/SolutionFiles/SolutionSample.java
+++ src/SolutionFiles/SolutionSample.java
@@ -1,7 +1,6 @@
 package SolutionFiles;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class SolutionSample {
@@ -9,27 +8,68 @@ public class SolutionSample {
 
     public static void main(String[] args) {
         int[] arr1 = {2, 4, 6, 8, 10};
-        int result = SolutionSample.getGCD(arr1, 5);
+        int result = SolutionSample.getGCD(2, "1A 2F 1C");
         System.out.println("the result is: " + result);
     }
 
-    static int getGCD(int[] arr, int num) {
-        List<Integer> x = Arrays.stream(arr).boxed().collect(Collectors.toList());
-        int gcd = 0;
-        int max = Arrays.stream(arr).reduce((acc, n) -> acc > n ? acc : n).getAsInt();
-        int counter = 1;
-        while (counter <= max) {
-            int j = 0;
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] % counter == 0) {
-                    j++;
-                }
-                if (j == arr.length) {
-                    gcd = counter;
-                }
+    static int getGCD(int N, String S) {
+
+        int totalSteats = 10;
+        int possibleWays = 0;
+
+        List<String> possibleSeatCombo = new ArrayList<>();
+        possibleSeatCombo.add("BCDE");
+        possibleSeatCombo.add("FGHJ");
+        possibleSeatCombo.add("DEFG");
+
+        if (!S.isEmpty()) {
+            String[] occupiedSeats = S.split(" ");
+            Map<Integer, List<String>> map = new HashMap<>();
+            for (int i = 0; i < N; i++) {
+                map.put(i, possibleSeatCombo);
             }
-            counter++;
+
+            //[1A, 2B]
+            String[] seatsBooked = S.split(" ");
+            for (String seat : seatsBooked) {
+                int rowNum = Integer.parseInt(seat.substring(0, 1)) - 1;
+                String seatLetter = seat.substring(1, 2);
+
+                for (int i = 0; i < map.get(rowNum).size(); i++) {
+
+                    map.get(rowNum).remove(validateMapHasLetter(map.get(rowNum), seatLetter));
+
+                }
+
+
+//                        map.get(rowNum).stream().filter(combos -> !combos.contains(seatLetter)).collect(Collectors.toList()));
+            }
+            System.out.println(map);
+        } else {
+            return 2 * N;
         }
-        return gcd;
+
+        return possibleWays;
+    }
+
+    private static Integer validateMapHasLetter(List<String> string, String seatLetter) {
+        for (int i = 0; i < string.size(); i++) {
+            if (string.get(i).contains(seatLetter)) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    private static int getIndex(List<String> string, String seatsInARow) {
+
+
+        for (int i = 0; i < string.size(); i++) {
+            if (string.get(i).contains(seatsInARow)) {
+                return i;
+            }
+        }
+        return 0;
     }
 }
